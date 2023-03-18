bd1b6146153f0d20e829fb246989e2c5b9a23778
joão Sátiro Bompastor <124584556+JoaoBompastor@users.noreply.github.com>
2023-03-18 14:02:41 -0300

    Update main.py

--- PokemonDBA/main.py
+++ PokemonDBA/main.py
@@ -3,6 +3,7 @@
 import discord
 from discord.ext import commands
 from util.config import TOKEN
+import util.Embed_Bonitinho
 
 
 # main
@@ -38,52 +39,64 @@ async def ping(ctx):
     await ctx.message.reply('pong!')
 
 
+# Defenders
+
 @bot.command()
 async def slowbro(ctx):
     print('Comando slowbro chamado')
-    await ctx.send('comando slowbro.')
+    # await ctx.send('comando slowbro.')
 
     embed = discord.Embed(title='slowbro :D',
-                          description='Guia altamente mecanico ultra informativo sobre builds de bonecos legais')
+                          description='Guia altamente mecanico ultra informativo sobre builds de bonecos legais',
+                          color=0x2ecc71)
 
     embed.set_thumbnail(url='https://d275t8dp8rxb42.cloudfront.net/pokemon/portrait/Slowbro.png')
-    embed.add_field(name='PLACEHOLDER', value='primeiro item', inline=True)
-    embed.add_field(name='PLACEHOLDER', value='segundo item', inline=True)
-    embed.add_field(name='PLACEHOLDER', value='terceiro item', inline=True)
+    embed.add_field(name='Build 1', value='Exp Share\nFocus Band\nAeos Cookie\n-----\nSurf/Amnesia', inline=True)
+    embed.add_field(name='Build 2', value='Focus Band\nAeos Cookie\nChoice Specs(ou Stack)\n-----\nScald/Telekinesis', inline=True)
+    embed.add_field(name='Build do Aramaki', value='Choice Specs\nOculos de Stack\nColher\n-----\nScald/Amnesia', inline=True)
+    embed.set_footer(text='Battle item: Potion', icon_url='https://github.com/JoaoBompastor/PokemonDBA/blob/main/PokemonDBAAssets/Potion.png?raw=true')
 
     await ctx.send(embed=embed)
 
+
+# Attackers
 
 @bot.command()
 async def sylveon(ctx):
     print('Comando sylveon chamado')
-    await ctx.send('comando sylveon.')
+    # await ctx.send('comando sylveon.')
 
     embed = discord.Embed(title='sylveon :)',
-                          description='Guia altamente mecanico ultra informativo sobre builds de bonecos legais')
+                          description='Guia altamente mecanico ultra informativo sobre builds de bonecos legais',
+                          color=0xe74c3c)
 
     embed.set_thumbnail(url='https://d275t8dp8rxb42.cloudfront.net/pokemon/thumbnail/Sylveon.png')
-    embed.add_field(name='PLACEHOLDER', value='primeiro item', inline=True)
-    embed.add_field(name='PLACEHOLDER', value='segundo item', inline=True)
-    embed.add_field(name='PLACEHOLDER', value='terceiro item', inline=True)
+    embed.add_field(name='Build 1', value='Choice Specs\nWise Glasses\nFocus Band\n-----\nHyper Voice/Calm Mind', inline=True)
+    embed.add_field(name='Build 2', value='Muscle Band\nScarf\nFocus Band\n-----\nMystical Fire/Draining Kiss', inline=True)
+    embed.add_field(name='Build do Aramaki', value='Muscle\nRazor\nScope\n-----\nMystical Fire/Calm Mind', inline=True)
+    embed.set_footer(text='Battle Item: Eject Button', icon_url='https://github.com/JoaoBompastor/PokemonDBA/blob/main/PokemonDBAAssets/Eject+Button.png?raw=true')
 
     await ctx.send(embed=embed)
 
+
+# Suportes
 
 @bot.command()
 async def mime(ctx):
     print('Comando mime chamado')
-    await ctx.send('comando mime.')
+    # await ctx.send('comando mime.')
 
-    embed = discord.Embed(title='coringa37',
-                          description='Guia altamente mecanico ultra informativo sobre builds de bonecos legais')
+    embed = discord.Embed(title='Mr. mime',
+                          description='Guia informativo osbre o coringa37(Mr. mime)', 
+                          color=0xf1c40f)
 
     embed.set_thumbnail(url='https://d275t8dp8rxb42.cloudfront.net/pokemon/thumbnail/Mr.Mime.png')
-    embed.add_field(name='PLACEHOLDER', value='primeiro item', inline=True)
-    embed.add_field(name='PLACEHOLDER', value='segundo item', inline=True)
-    embed.add_field(name='PLACEHOLDER', value='terceiro item', inline=True)
+    embed.add_field(name='Build 1', value='Aeos cookie\nFocus band\nEnergy amp\n-----\nCordinha/psychic', inline=True)
+    embed.add_field(name='Build 2', value='Choice Specs\nColher\nFocus Band\n-----\nConfusion/Barreira', inline=True)
+    embed.add_field(name='Build do Aramaki', value='Exp Share\nFocus Band\nBuddy Barrier\n-----\nPsychic/Barreira', inline=True)
+    embed.set_footer(text='Battle Item: Xspeed', icon_url='https://github.com/JoaoBompastor/PokemonDBA/blob/main/PokemonDBAAssets/X+Speed.png?raw=true')
 
-    await ctx.send(embed=embed)
+    await ctx.message.reply(embed=embed)
 
 
 # Util Commands
@@ -104,6 +117,15 @@ async def ajuda(ctx):
     await ctx.send(embed=embed)
 
 
+# Comando inoperante por enquanto
+
+@bot.command()
+async def teste(ctx):
+    init = util.Embed_Bonitinho.Embed_bonitinho('pokemon', 'titulo', ctx=ctx)
+
+    await ctx.send(embed=init)
+
+
 @bot.command()
 async def info(ctx):
     print('Comando info chamado')
@@ -113,7 +135,11 @@ async def info(ctx):
                           color=0x992d22)
 
     embed.set_thumbnail(url='https://github.com/JoaoBompastor/PokemonDBA/blob/main/PokemonDBAAssets/five_sem_bg.png?raw=true')
-    embed.set_author(name='Coringa37', url='https://github.com/JoaoBompastor', icon_url='https://github.com/JoaoBompastor/PokemonDBA/blob/main/PokemonDBAAssets/bannerserio.jpg?raw=true')
+
+    embed.set_author(name='Coringa37', 
+                    url='https://github.com/JoaoBompastor',
+                    icon_url='https://github.com/JoaoBompastor/PokemonDBA/blob/main/PokemonDBAAssets/bannerserio.jpg?raw=true')
+
     embed.set_footer(text='footer')
 
     await ctx.send(embed=embed)

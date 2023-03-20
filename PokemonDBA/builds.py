7024b0aca6ad6cc1c9fa63bfa38254bb0af93851
joão Sátiro Bompastor <124584556+JoaoBompastor@users.noreply.github.com>
2023-03-20 20:53:40 -0300

    Add files via upload

---
+++ PokemonDBA/builds.py
@@ -0,0 +1,72 @@
+class buildsshow:
+    lista_builds: list = ['slowbro', 'sylveon', 'mime', 'gardevoir', 'glaceon', 'gengar', 'garchomp',
+                          'espeon', 'espeon', 'eldegoss', 'duraludon', 'dragapult', 'dragonite', 
+                          'dodrio', 'delphox']
+
+    # build slowbro
+
+    slowbro1: str = 'Exp Share\nFocus Band\nAeos Cookie\n-----\nSurf/Amnesia'
+    slowbro2: str = 'Focus Band\nAeos Cookie\nChoice Specs(ou Stack)\n-----\nScald/Telekinesis'
+    slowbro_aramaki: str = 'Choice Specs\nOculos de Stack\nSlick Spoon\n-----\nScald/Amnesia'
+
+    # build sylveon
+
+    sylveon1:str = 'Choice Specs\nWise Glasses\nFocus Band\n-----\nHyper Voice/Calm Mind'
+    sylveon2:str = 'Muscle Band\nScarf\nFocus Band\n-----\nMystical Fire/Draining Kiss'
+    sylveon_aramaki:str = 'Muscle\nRazor\nScope\n-----\nMystical Fire/Calm Mind'
+
+    # build mime
+
+    mime1: str = 'Aeos cookie\nFocus band\nEnergy amp\n-----\nCordinha/psychic'
+    mime2: str = 'Choice Specs\nSlick Spoon\nFocus Band\n-----\nConfusion/Barreira'
+    mime_aramaki: str = 'Exp Share\nFocus Band\nBuddy Barrier\n-----\nPsychic/Barreira'
+
+    #build garde
+
+    gardevoir1: str = 'Slick Spoon\nWise Glasses\nChoice Specs\n-----\nPsychock/Psychic'
+
+    # build glaceon
+
+    glaceon1: str = 'Choice Specs\nSlick Spoon\nEnergy Amp\n-----\nIcy Wind/Freeze Dry'
+
+    # build gengar
+
+    gengar1: str = 'Choice Specs\nWise Glasses\nSp. Attack Specs\n-----\nDream Eater/Shadow Ball'
+
+    # build garchomp
+
+    chomp1: str = 'Muscle Band\nScarf\nScope Lens\n-----\nDragon Rush/Dragon Claw'
+
+    # build espeon
+
+    espeon1: str = 'Choice Specs\nSlick Spoon\nFocus Band\n-----\nPsyshock/Psybeam'
+
+    # build eldegoss
+
+    eldegoss1: str = 'Muscle Band\nExp. Share \nBuddy Barrier\n-----\nPollen Puff/Cotton Spore'
+
+    # build duraludon
+
+    poligono1: str = 'Muscle Band\nScarf\nScope Lens\n-----\nDragon Pulse/Dragon Claw'
+
+    # build dragonite
+
+    dragonite1: str = 'Muscle Band\nScarf\nFocus Band\n-----\nDragon Dance/Hyper Beam'
+
+    # build dragapult
+
+    dragapult1: str = 'Muscle Band\nScarf\nScope Lens\n-----\nDragon Dance/Dragon Breath'
+
+    # build dodrio
+
+    dodrio1: str = 'Attack Weight\nAeos Cookie\nRazor Claw\n-----\nDrill Peck/Jump-Kick'
+
+    # build delphox
+
+    delphox1: str = 'Choice Specs\nEnergy Amp\nShell Bell\n-----\nMystical Fire/Flame Charge'
+
+class items:
+    eject: str = 'https://github.com/JoaoBompastor/PokemonDBA/blob/main/PokemonDBAAssets/Eject+Button.png?raw=true'
+    xspeed: str = 'https://github.com/JoaoBompastor/PokemonDBA/blob/main/PokemonDBAAssets/X+Speed.png?raw=true'
+    potion: str = 'https://github.com/JoaoBompastor/PokemonDBA/blob/main/PokemonDBAAssets/Potion.png?raw=true'
+    

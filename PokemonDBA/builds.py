5dd3a3224698ad745f5480a24e3e2e683f4b860b
joão Sátiro Bompastor <124584556+JoaoBompastor@users.noreply.github.com>
2023-04-02 23:07:55 -0300

    Update builds.py
    novo: classe cores adicionada, com funcao de facilitar a chamada individual de cores do main.py
    pokemons adicionados: mew, mamosiwne, pikachu, scyther, scizor, snorlax, sableye

--- PokemonDBA/builds.py
+++ PokemonDBA/builds.py
@@ -11,9 +11,9 @@ class buildsshow:
 
     # build sylveon
 
-    sylveon1:str = 'Choice Specs\nWise Glasses\nFocus Band\n-----\nHyper Voice/Calm Mind'
-    sylveon2:str = 'Muscle Band\nScarf\nFocus Band\n-----\nMystical Fire/Draining Kiss'
-    sylveon_aramaki:str = 'Muscle\nRazor\nScope\n-----\nMystical Fire/Calm Mind'
+    sylveon1: str = 'Choice Specs\nWise Glasses\nFocus Band\n-----\nHyper Voice/Calm Mind'
+    sylveon2: str = 'Muscle Band\nScarf\nFocus Band\n-----\nMystical Fire/Draining Kiss'
+    sylveon_aramaki: str = 'Muscle\nRazor\nScope\n-----\nMystical Fire/Calm Mind'
 
     # build mime
 
@@ -21,7 +21,7 @@ class buildsshow:
     mime2: str = 'Choice Specs\nSlick Spoon\nFocus Band\n-----\nConfusion/Barreira'
     mime_aramaki: str = 'Exp Share\nFocus Band\nBuddy Barrier\n-----\nPsychic/Barreira'
 
-    #build garde
+    # build garde
 
     gardevoir1: str = 'Slick Spoon\nWise Glasses\nChoice Specs\n-----\nPsychock/Psychic'
 
@@ -65,8 +65,75 @@ class buildsshow:
 
     delphox1: str = 'Choice Specs\nEnergy Amp\nShell Bell\n-----\nMystical Fire/Flame Charge'
 
+    # build goodra
+
+    goodra1: str = 'Muscle Band\nFocus Band\nBuddy Barrier\n-----\nDragon Pulse/Power Whip'
+
+    # build greedent
+
+    theodor1: str = 'Attack Weight\nFocus Band\nScore Shield\n-----\nCovet/Belch'
+
+    # build greninja
+
+    greninja1: str = 'Muscle Band\nRapid-fire Scarf\nAttack Weight\n-----\nSmokescreen/Surf'
+
+    # buld hoopa
+
+    hoopa1: str = 'Muscle Band\nFocus Band\nExp. Share\n-----\nHyperspace Hole/Phantom Force'
+
+    # build lucario
+
+    lucario1: str = 'Attack Weight \nRazor Claw\nMuscle Band\n-----\nExtreme Speed/Bone Rush'
+
+    # build machamp
+
+    machamp1: str = 'Attack Weight \nRazor Claw\nScope Lens\n-----\nDynamic Punch/Cross Chop'
+
+    # dia 2/4
+
+    # build mamute
+
+    mamute1: str = 'Exp. Share\nFocus Band\nBuddy Barrier\n-----\nIcicle Crash/Highhorse Power'
+
+    # build mew
+
+    mew1: str = 'Choice Specs\nSp. Attack Specs\nWise Glasses'
+
+    # build pikachu
+
+    pikachu1: str = 'Muscle Band\nFocus Band\nBuddy Barrier\n-----\nElectro Ball/Volt Tackle'
+
+    # build sableye
+
+    sableye1: str = 'Attack Weight\nRazor Claw\nFloat Stone\n-----\nShadow Sneak/Confuse Ray'
+
+    # build scizor
+
+    scizor1: str = 'Attack Weight\nFocus Band\nWeakness Policy\n-----\nBullet Punch/Sword Dance'
+
+    # build scyther
+
+    scyther1: str = 'Attack Weight\nRazor Claw\nFocus Band\n-----\nDual Wingbeat/Double Hit'
+
+    # build snorlax
+
+    snorlax1: str = 'Exp. Share\nFocus Band\nBuddy Barrier\n-----\nHeavy Slam/Block'
+
+
 class items:
     eject: str = 'https://github.com/JoaoBompastor/PokemonDBA/blob/main/PokemonDBAAssets/Eject+Button.png?raw=true'
     xspeed: str = 'https://github.com/JoaoBompastor/PokemonDBA/blob/main/PokemonDBAAssets/X+Speed.png?raw=true'
+    xattack: str = 'https://github.com/JoaoBompastor/PokemonDBA/blob/main/PokemonDBAAssets/X+Attack.png?raw=true'
     potion: str = 'https://github.com/JoaoBompastor/PokemonDBA/blob/main/PokemonDBAAssets/Potion.png?raw=true'
-    
+    full_heal: str = 'https://github.com/JoaoBompastor/PokemonDBA/blob/main/PokemonDBAAssets/Full+Heal.png?raw=true'
+    Goal_Getter: str = 'https://github.com/JoaoBompastor/PokemonDBA/blob/main/PokemonDBAAssets/Goal+Getter.png?raw=true'
+
+
+# 4/2
+
+class cores:
+    Attackers: int = 0xe74c3c
+    Defenders: int = 0x2ecc71
+    Supports: int = 0xf1c40f
+    Speedsters: int = 0x3498db
+    AllRounders: int = 0x9b59b6
